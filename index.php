dac23bc27ee5b50ad617d9b5e5cd3921fbccb163
ONEHANDUZ <onehanduz@gmail.com>
2022-01-05 13:01:05 +0500

    index file update

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 // API_TOKEN
-include 'api.php';//metodlar
+include './api.php';//metodlar
 define('API_KEY',$TOKEN);
 // INCLUDES
-include 'functions.php';//metodlar
-include 'values.php';//o'zgaruvchilar
-include 'keyboards.php';//keyboardlar
-include 'texts.php';//textlari
-include 'mysql.inc.php';//mysql
+include './functions.php';//metodlar
+include './values.php';//o'zgaruvchilar
+include './keyboards.php';//keyboardlar
+include './texts.php';//textlari
+include './mysql.inc.php';//mysql
 
 //BASIC CMD
 if($txt=="/start"){
     sendmessage($cid,$t_start,$k_remove);
 }
-           
+echo "test";           
 ?>
 <h1s>Manzil : @onehanduz</h1>
