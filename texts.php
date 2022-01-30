87bb041483e88464e67ae1bd3506df7195ceecdc
ONEHANDUZ <onehanduz@gmail.com>
2022-01-30 23:58:55 +0500

    v1.0

--- texts.php
+++ texts.php
@@ -1,5 +1,5 @@
 <?php
 $t_start = "Assalomu alaykum $first_name $last_name.
 Menga videoni yuboring men videoning hajmini kamaytirib beraman(videoning hajmi bilan birga uning sifati ham pasayadi).";
-$t_video = "Farmatini tanlang:";
+$t_video = "Farmatini tanlang $getfile:";
 ?>
